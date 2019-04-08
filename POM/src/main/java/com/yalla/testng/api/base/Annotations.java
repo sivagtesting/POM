230728bbaef380bff17b05a000bbac4f46c42ef2
sivagtesting <47685173+sivagtesting@users.noreply.github.com>
2019-04-08 18:49:29 +0530

    all POM project related

--- POM/src/main/java/com/yalla/testng/api/base/Annotations.java
+++ POM/src/main/java/com/yalla/testng/api/base/Annotations.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+
 import com.yalla.selenium.api.base.SeleniumBase;
 
 import utils.DataLibrary;
